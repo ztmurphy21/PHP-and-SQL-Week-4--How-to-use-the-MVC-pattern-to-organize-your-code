477167a5b46059d7b99b96fe8eb192f4988ebc30
Zachary Murphy <murphyz@my.wccc.edu>
2016-09-20 19:56:17 -0400

    Chapter 5 work

---
+++ ch05_ex1/view/categories_nav.php
@@ -0,0 +1,12 @@
+<nav>
+    <ul>
+         <?php foreach($categories as $category) : ?>
+            <li>
+               <a href="?category_id=<?php 
+                  echo $category['categoryID']; ?>">
+        <?php echo $category['categoryName']; ?>
+                 </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</nav>
